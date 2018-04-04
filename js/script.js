02ea93eab2c5a6c881d15108d58c3a1ac37b6b4c
Emanoel Queiroz <emanoelqueiroz@gmail.com>
2018-04-03 21:21:33 -0300

    Ajustes e implementações tope

--- js/script.js
+++ js/script.js
@@ -1,17 +1,27 @@
-
 +function () {
+
+  // Button Elements
   const playEle = document.querySelector('.play');
+  const pauseEle = document.querySelector('.pause');
+  const gainEle = document.querySelector('.gain');
+  const distortionEle = document.querySelector('.distortion');
+
+  // Canvas and Audio Elements
   const canvasEle = document.querySelector('canvas');
   const audioEle = document.querySelector('audio');
   const canvasCtx = canvasEle.getContext('2d');
+
+  // Audio Context and Effects
   const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
   const source = audioCtx.createMediaElementSource(audioEle);
   const analyser = audioCtx.createAnalyser();
   const gain = audioCtx.createGain();
+  const dist = audioCtx.createWaveShaper();
 
-  source.connect(analyser);
-  source.connect(gain);
-  gain.connect(audioCtx.destination);
+  source.connect(dist);
+  dist.connect(gain);
+  gain.connect(analyser);
+  analyser.connect(audioCtx.destination);
 
   canvasEle.width = window.innerWidth;
   canvasEle.height = window.innerHeight;
@@ -26,24 +36,47 @@
     let x = 0;
     let barHeight;
     const barWidth = (WIDTH / bufferLength) * 2.5;
-    const drawVisual = requestAnimationFrame(draw);
-
-    analyser.getByteFrequencyData(dataArray);
 
     canvasCtx.fillStyle = '#222';
     canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
+    analyser.getByteFrequencyData(dataArray);
 
     for (i; i < bufferLength; i++) {
-      barHeight = dataArray[i] * 5;
+      barHeight = dataArray[i] * 3;
+      dataArray[i] = dataArray[i] - 5;
+      canvasCtx.fillStyle = `rgba(158, 67, 243, ${barHeight / 350})`;
 
-      canvasCtx.fillStyle = '#BF7BFF';
       canvasCtx.fillRect(x, HEIGHT - barHeight / 2, barWidth, barHeight);
-
       x += barWidth + 1;
     }
+
+    requestAnimationFrame(draw);
   }
 
-  draw();
+  function makeDistortionCurve(amount) {
+    let x;
+    let i = 0;
+    const k = Number(amount);
+    const n_samples = 44100;
+    const curve = new Float32Array(n_samples);
+    const deg = Math.PI / 180;
+    for (i; i < n_samples; i++) {
+      x = i * 2 / n_samples - 1;
+      curve[i] = (3 + k) * x * 20 * deg / (Math.PI + k * Math.abs(x));
+    }
+    return curve;
+  }
+
+  // Event Listeners
   playEle.addEventListener('click', () => audioEle.play());
+
+  pauseEle.addEventListener('click', () => audioEle.pause());
+
+  gainEle.addEventListener('input', () => gain.gain.value = gainEle.value);
+
+  distortionEle.addEventListener('input', () => dist.curve = makeDistortionCurve(distortionEle.value));
+
+  // Start All
+  draw();
 
 }();
