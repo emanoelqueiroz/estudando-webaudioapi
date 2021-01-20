b8d303cba796f8a84089f5ade1c200518b253898
Emanoel Queiroz <emanoelqueiroz@gmail.com>
2021-01-20 16:51:40 -0300

    ajuste para pegar a voz do usuário

--- js/script.js
+++ js/script.js
@@ -1,102 +1,111 @@
 +function () {
 
-  // Button and Input Elements
-  const playEle = document.querySelector('.play');
-  const pauseEle = document.querySelector('.pause');
-  const gainEle = document.querySelector('.gain');
-  const distortionEle = document.querySelector('.distortion');
-  const urssModeEle = document.querySelector('.urss-mode');
+  navigator.getUserMedia = navigator.getUserMedia
+    || navigator.webkitGetUserMedia
+    || navigator.mozGetUserMedia;
 
-  // Canvas, Img and Audio Elements
-  const canvasEle = document.querySelector('canvas');
-  const audioEle = document.querySelector('audio');
-  const img = document.querySelector('.stalin');
-  const canvasCtx = canvasEle.getContext('2d');
+  navigator.getUserMedia({ video: false, audio: true }, callback, console.log);
 
-  // Audio Context and Effects
-  const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-  const source = audioCtx.createMediaElementSource(audioEle);
-  const analyser = audioCtx.createAnalyser();
-  const gain = audioCtx.createGain();
-  const dist = audioCtx.createWaveShaper();
+  function callback(stream) {
 
-  source.connect(dist);
-  dist.connect(gain);
-  gain.connect(analyser);
-  analyser.connect(audioCtx.destination);
+    // Button and Input Elements
+    const playEle = document.querySelector('.play');
+    const pauseEle = document.querySelector('.pause');
+    const gainEle = document.querySelector('.gain');
+    const distortionEle = document.querySelector('.distortion');
+    const urssModeEle = document.querySelector('.urss-mode');
 
-  canvasEle.width = window.innerWidth;
-  canvasEle.height = window.innerHeight;
+    // Canvas, Img and Audio Elements
+    const canvasEle = document.querySelector('canvas');
+    const audioEle = document.querySelector('audio');
+    const img = document.querySelector('.stalin');
+    const canvasCtx = canvasEle.getContext('2d');
 
-  let showImg = false;
-  let backgroundColor = '#222';
-  let barColor = 'rgba(158, 67, 243, $s)';
-  const WIDTH = canvasEle.width;
-  const HEIGHT = canvasEle.height;
-  const bufferLength = analyser.frequencyBinCount;
-  const dataArray = new Uint8Array(bufferLength);
+    // Audio Context and Effects
+    const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+    const source = audioCtx.createMediaStreamSource(stream);
+    const analyser = audioCtx.createAnalyser();
+    const gain = audioCtx.createGain();
+    const dist = audioCtx.createWaveShaper();
 
-  function draw() {
-    let i = 0;
-    let x = 0;
-    let barHeight;
-    const barWidth = (WIDTH / bufferLength) * 2.5;
+    source.connect(dist);
+    dist.connect(gain);
+    gain.connect(analyser);
+    analyser.connect(audioCtx.destination);
 
-    requestAnimationFrame(draw);
+    canvasEle.width = window.innerWidth;
+    canvasEle.height = window.innerHeight;
 
-    canvasCtx.fillStyle = backgroundColor;
-    canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
-    analyser.getByteFrequencyData(dataArray);
-    
-    if (showImg) canvasCtx.drawImage(img, (WIDTH / 2) - (img.width / 2), (HEIGHT / 2) - (img.height / 2));
+    let showImg = false;
+    let backgroundColor = '#222';
+    let barColor = 'rgba(158, 67, 243, $s)';
+    const WIDTH = canvasEle.width;
+    const HEIGHT = canvasEle.height;
+    const bufferLength = analyser.frequencyBinCount;
+    const dataArray = new Uint8Array(bufferLength);
 
-    for (i; i < bufferLength; i++) {
-      barHeight = dataArray[i] * 3;
-      dataArray[i] = dataArray[i] - 5;
+    function draw() {
+      let i = 0;
+      let x = 0;
+      let barHeight;
+      const barWidth = (WIDTH / bufferLength) * 2.5;
 
-      canvasCtx.fillStyle = barColor.replace('$s', barHeight / 350);
-      canvasCtx.fillRect(x, HEIGHT - barHeight / 2, barWidth, barHeight);
+      requestAnimationFrame(draw);
 
-      x += barWidth + 1;
+      canvasCtx.fillStyle = backgroundColor;
+      canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
+      analyser.getByteFrequencyData(dataArray);
+
+      if (showImg) canvasCtx.drawImage(img, (WIDTH / 2) - (img.width / 2), (HEIGHT / 2) - (img.height / 2));
+
+      for (i; i < bufferLength; i++) {
+        barHeight = dataArray[i] * 3;
+        dataArray[i] = dataArray[i] - 5;
+
+        canvasCtx.fillStyle = barColor.replace('$s', barHeight / 350);
+        canvasCtx.fillRect(x, HEIGHT - barHeight / 2, barWidth, barHeight);
+
+        x += barWidth + 1;
+      }
     }
-  }
 
-  function makeDistortionCurve(amount) {
-    let x;
-    let i = 0;
-    const k = Number(amount);
-    const n_samples = 44100;
-    const curve = new Float32Array(n_samples);
-    const deg = Math.PI / 180;
-    for (i; i < n_samples; i++) {
-      x = i * 2 / n_samples - 1;
-      curve[i] = (3 + k) * x * 20 * deg / (Math.PI + k * Math.abs(x));
+    function makeDistortionCurve(amount) {
+      let x;
+      let i = 0;
+      const k = Number(amount);
+      const n_samples = 44100;
+      const curve = new Float32Array(n_samples);
+      const deg = Math.PI / 180;
+      for (i; i < n_samples; i++) {
+        x = i * 2 / n_samples - 1;
+        curve[i] = (3 + k) * x * 20 * deg / (Math.PI + k * Math.abs(x));
+      }
+      return curve;
     }
-    return curve;
+
+    // Event Listeners
+    playEle.addEventListener('click', () => audioEle.play());
+
+    pauseEle.addEventListener('click', () => audioEle.pause());
+
+    gainEle.addEventListener('input', () => gain.gain.value = gainEle.value);
+
+    distortionEle.addEventListener('input', () => dist.curve = makeDistortionCurve(distortionEle.value));
+
+    urssModeEle.addEventListener('click', () => {
+      const { style } = document.body;
+
+      showImg = true;
+      backgroundColor = '#C00';
+      barColor = 'rgba(255, 215, 0, $s)';
+
+      style.setProperty('--primary-color', '#C00');
+      style.setProperty('--secondary-color', '#FFD700');
+      style.setProperty('--text-color', '#FFD700');
+    });
+
+    // Start All
+    draw();
   }
-
-  // Event Listeners
-  playEle.addEventListener('click', () => audioEle.play());
-
-  pauseEle.addEventListener('click', () => audioEle.pause());
-
-  gainEle.addEventListener('input', () => gain.gain.value = gainEle.value);
-
-  distortionEle.addEventListener('input', () => dist.curve = makeDistortionCurve(distortionEle.value));
-
-  urssModeEle.addEventListener('click', () => {
-    const { style } = document.body;
-
-    showImg = true;
-    backgroundColor = '#C00';
-    barColor = 'rgba(255, 215, 0, $s)';
-
-    style.setProperty('--primary-color', '#C00');
-    style.setProperty('--secondary-color', '#FFD700');
-    style.setProperty('--text-color', '#FFD700');
-  });
-
-  // Start All
-  draw();
 
 }();
